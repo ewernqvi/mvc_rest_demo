50d957d27ce583d804cd3a8d642dd902741ce5aa
Erik Wernqvist <erik@wernqvist.eu>
2014-02-06 14:50:50 +0100

    added logic to handle backend

--- server/public/app/js/services.js
+++ server/public/app/js/services.js
@@ -53,72 +53,33 @@ function user($http){
 
 function advertisment($http, $q){
   function list(){
-        return [
-                {
-                _id: "dummy-client-id1",
-                category: "Hobbies",
-                created: "2014-02-04T09:27:34.825Z",
-                description: "Premium Surf Board",
-                images: [
-                         {
-                         contentType: "image/jpg",
-                         advertismentId: "dummy-client-id1",
-                         href: "https://raw.github.com/ewernqvi/mvc_rest_demo/master/server/test-data/img/surfboard.jpg"
-                         }
-                         ],
-                owner: "mrx@gmail.com",
-                price: "$110"
-                },
-                {
-                _id: "dummy-client-id2",
-                category: "Hobbies",
-                created: "2014-02-11T09:27:34.825Z",
-                description: "Premium Long Board",
-                images: [
-                         {
-                         contentType: "image/jpg",
-                         advertismentId: "dummy-client-idr2",
-                         href: "https://raw.github.com/ewernqvi/mvc_rest_demo/master/server/test-data/img/longboard.jpg"
-                         }
-                         ],
-                owner: "mrx@gmail.com",
-                price: "$220"
-                },
-                {
-                _id: "dummy-client-id3",
-                category: "Hobbies",
-                created: "2014-02-10T09:27:34.825Z",
-                description: "Dr Zoggs Sex Wax",
-                images: [
-                         {
-                         contentType: "image/jpg",
-                         advertismentId: "dummy-client-id3",
-                         href: "https://raw.github.com/ewernqvi/mvc_rest_demo/master/server/test-data/img/zoggs.jpg"
-                         }
-                         ],
-                owner: "mrx@gmail.com",
-                price: "$11"
-                }
-                ];
+    // the $http API is based on the deferred/promise APIs exposed by the $q service
+    // so it returns a promise for us by default
+    return $http.get('/api/advertisments').then(function(response) {
+      if (typeof response.data === 'object') {
+        return response.data;
+      }else{
+        // invalid response
+        return $q.reject(response.data);
+      }
+    }, function(err) {
+      // something went wrong
+      return $q.reject(err.data);
+    });
   }
   return{
     get: function(adId){
-      var deferred = $q.defer();
-      var res=null;
-      // Dummy implementation for now, we shall call the server
-      var l=list();
-      for(var i=0; i < l.length; i++){
-        if(l[i]._id === adId){
-          res = l[i];
-          break;
-        }
+     return $http.get('/api/advertisments/' + adId).then(function(response) {
+      if (typeof response.data === 'object') {
+        return response.data;
+      }else{
+        // invalid response
+        return $q.reject(response.data);
       }
-      if(res)
-        deferred.resolve(res); 
-      else 
-        deferred.reject('Advertisment with id: ' + adId + ' not found!'); 
-      
-      return deferred.promise;
+    }, function(err) {
+      // something went wrong
+      return $q.reject(err.data);
+    });
       
     },
     list: list
