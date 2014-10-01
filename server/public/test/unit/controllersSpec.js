e6b57e0c68b383d851479b21851876c2dbd812e3
Erik Wernqvist <erik@wernqvist.eu>
2014-10-01 10:24:31 +0200

    staged non-working test

--- server/public/test/unit/controllersSpec.js
+++ server/public/test/unit/controllersSpec.js
@@ -2,35 +2,64 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
-  // our controller
+describe('controllers', function(){ 
+// Test for AdvertismentsCtrl
   describe('AdvertismentsCtrl', function(){
-    var scope, ctrl,q, rs;
+    var scope, ctrl, httpMock;
     beforeEach(module('buyAndSellApp'));
 
-    beforeEach(inject(function($controller, advertisment, $q, $rootScope) {
+    beforeEach(inject(function($controller, $injector) {
       scope = {};
-      q=$q;
-      rs=$rootScope;
+      httpMock = $injector.get('$httpBackend');
+      httpMock.expectGET("/api/advertisments").respond([1,3,4]);
       ctrl = $controller('AdvertismentsCtrl', {$scope:scope});
     })); 
 
-    it('should create advertisment model with 3 advertisments', function($controller) {
-      // make the promise fire
-      rs.$apply();
-      //Check that the controller has a list of three advertisments
-      
-      
+    it('should create advertisment model with 3 advertisments', function() {
+      httpMock.flush();
       expect(scope.advertisments.length).toBe(3);
-    })
+      var res = scope.formatDate(new Date());
+      //expect(res.match(/today/).index).toBe(0);
+    });
   });
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+  // Test for AdvertismentDetailCtrl
+  describe('AdvertismentDetailCtrl', function(){
+    var scope, ctrl, httpMock;
+    var adId = 'testId';
+    beforeEach(module('buyAndSellApp'));
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+    beforeEach(inject(function($controller, $httpBackend, $routeParams) {
+      scope = {};
+      var route = {};
+      httpMock = $httpBackend;
+      var d = new Date();
+      $routeParams.id = adId;
+      httpMock.expectGET("/api/advertisments/testId").respond(
+        {_id:adId, created: d, price: '35',description: 'test-part', 
+          images: [{a:'a'}]});
+      ctrl = $controller('AdvertismentDetailCtrl', {$scope:scope});
+    })); 
+
+    it('should return an advertisment', function() {
+      if(httpMock && httpMock.flush) {
+        console.log('**' + httpMock.flush);
+        httpMock.flush();
+      }
+      else console.log('what the fuck..');
+      expect(scope.advertisment.description).toBe('test-part');
+    });
+
+    it('should set advertisment id', function() {
+      //httpMock.flush();
+      expect(scope.adId).toBe(adId);
+    });
+
+    it('should contain function formatDate', function(){
+      //httpMock.flush();
+      expect(typeof scope.formatDate).toBe('function');
+    });
+
+  });
 
 });
